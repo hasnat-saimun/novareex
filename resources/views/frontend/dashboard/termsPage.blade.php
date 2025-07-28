0aa9ea9e5b217b440122e16548ce611985c84af3
vitprofessional <46179916+vitprofessional@users.noreply.github.com>
2025-07-28 19:38:01 +0600

    Update termsPage.blade.php

--- resources/views/frontend/dashboard/termsPage.blade.php
+++ resources/views/frontend/dashboard/termsPage.blade.php
@@ -163,7 +163,7 @@
                             <ul>
                                 <li><strong>Company Name:</strong> Nuvareex Trading LLC</li>
                                 <li><strong>Incorporated In:</strong> St. Vincent & the Grenadines (SVG)</li>
-                                <li><strong>License Number:</strong> 4243LLC2025</li>
+                                <li><strong>Registration number</strong> 4243LLC2025</li>
                                 <li><strong>Registered Address:</strong> Suite 305, Griffth Corporate Center, Beachmont, Kingstown, SVG 1510</li>
                                 <li><strong>Email:</strong> <a href="mailto:nuvareextrading@gmail.com">nuvareextrading@gmail.com</a></li>
                                 <li><strong>Mobile:</strong> +971566238304</li>
